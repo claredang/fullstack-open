456a130ce1e1793285210808c041df06dfb6ceca
claredang <dpthuy2000@gmail.com>
2021-09-03 13:31:54 -0700

    until 3.12

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const { response } = require('express')
 const express = require('express')
 const app = express()
-
+const Person = require('./models/person')
 app.use(express.json())
 
 let persons = [
@@ -27,6 +28,7 @@ let persons = [
       }
   ]
 
+// Fetching random string response
 //   app.get('/', (request, response) => {
 //     response.send('<h1>Hello World!</h1>')
 //   })
@@ -50,52 +52,46 @@ app.get('/info',  (request, response) => {
     )
 })
 
-  app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response) => {
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
   })
+})
   
-  // Add new entries
-  const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
+// Add new entries
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' 
+    })
   }
 
-  app.post('/api/persons', (request, response) => {
-    const body = request.body
-  
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
-    }
-    const person = {
-        name: "Kwon Ji Yong",
-        id: generateId(),
-        number: Array.from({length: 9}, () => Math.floor(Math.random() * 9))
-      }
-    
-      persons = persons.concat(person)
-    
-      response.json(person)
-    })
+  const person = {
+    name: body.name,
+    number: body.number
+  }
 
-  app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
   })
-  
-  const PORT = 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const [ , , password, name, number] = process.argv // input for command line: node mongo.js 
+const dbName = 'phonebook'
+
+if (!password) {
+  console.log('missing password as argument')
+  process.exit(1)
+}
+
+const url =
+`mongodb+srv://clare:${password}@cluster0.2v4nc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true })
+  .catch(error => console.error('DB connect error:', error))
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const findPerson = () => {
+  Person.find({}).then(result => {
+    console.log('Phonebook database:')
+    result.forEach(p => console.log(`${p.name} ${p.number}`))
+    mongoose.connection.close()
+  })
+}
+
+const addPerson = () => {
+  const person = new Person({
+    name,
+    number
+  })
+
+person.save().then(() => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
+
+// 3.12
+(name && number) ? addPerson() : findPerson()
